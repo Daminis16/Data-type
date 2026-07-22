b2d0ef8d544573bff84c795b60b69afdaac74337
Damini <daminisharnagate01234@gmail.com>
2026-07-22 22:47:20 +0530

    Add files via upload

---
+++ 02_datatypes.py
@@ -0,0 +1,15 @@
+a=2 #a is integer
+b=98.23 #b is float
+c="Hello" #c is string
+d=1+2j #d is complex number
+e=True #e is boolean
+f=None #f is NoneType
+
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
